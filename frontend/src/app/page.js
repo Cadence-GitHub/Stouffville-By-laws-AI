79b22f9055c184fbd587cb2f07418e6118509f38
Franz Luiz Sy <luisitooo@luisitooo.local>
2025-03-29 08:20:26 -0400

    front-end env setup

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      Stouffville By-laws AI Project!
+    </div>
+  );
+}
